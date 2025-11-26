9e6974768052e1c91ec4d3d1e7f0d037cda01532
Hugo Ohlsson <hugoohlsson02@hotmail.com>
2025-11-26 14:54:23 +0100

    26/11 small changes

--- SearchAndRescueRobot/include/LineSensor.h
+++ SearchAndRescueRobot/include/LineSensor.h
@@ -2,19 +2,22 @@
 #include <QTRSensors.h>
 #include <Arduino.h>
 #include <PID_v1.h>
+#include "MotorDriver.h"
 
 class LineSensor{
     public:
      LineSensor();
-     void begin(uint8_t arrayPin1, uint8_t arrayPin2, uint8_t arrayPin3, uint8_t arrayPin4, uint8_t singlePin);
+     void begin(uint8_t arrayPin1, uint8_t arrayPin2, uint8_t arrayPin3, uint8_t arrayPin4, uint8_t singlePin, MotorDriver motor);
      void calibrate();
-     int followLine();
+     void followLine();
      String checkJunction();
 
     private:
     QTRSensors qtr;
     uint16_t _sensors[4];
     int16_t _position;
+    int16_t _lSpeed;
+    int16_t _rSpeed;
     PID pid;
     double _output;
     double _input;
--- SearchAndRescueRobot/src/main.cpp
+++ SearchAndRescueRobot/src/main.cpp
@@ -8,26 +8,17 @@ MotorDriver motor;
 LineSensor lineSensor;
 
 
-void setup() {
 
-  motor.begin(MOTOR_PIN_M1A,MOTOR_PIN_M1B,MOTOR_PIN_M2A,MOTOR_PIN_M2B);
-  lineSensor.begin(IR_ARRAY_PIN1,IR_ARRAY_PIN2,IR_ARRAY_PIN3,IR_ARRAY_PIN4, 1);
+void setup()
+{
+
+  motor.begin(MOTOR_PIN_M1A, MOTOR_PIN_M1B, MOTOR_PIN_M2A, MOTOR_PIN_M2B);
+  lineSensor.begin(IR_ARRAY_PIN1, IR_ARRAY_PIN2, IR_ARRAY_PIN3, IR_ARRAY_PIN4, 1);
   lineSensor.calibrate();
   Serial.begin(9600);
-  
-  
 }
 
-
-void loop() {
- int position = lineSensor.followLine();
- if(position > 1000 && position < 2500){
-  motor.drive(50,50);
- } else if(position <1000){
-  motor.drive(50, 15);
-
- } else if(position >2000){
-  motor.drive(15,50);
- }
-    
+void loop()
+{
+  lineSensor.followLine();
 }
